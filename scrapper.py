dbea07629b254b12d73b74ca33e42e8ddb9c68ff
Juan Gabriel Valdés Díaz <jgvaldes1992@gmail.com>
2022-08-26 20:51:43 -0400

    Adding 100 MLC estimate by each coind, adding total cup for each coin, update text messages, updating environment template

--- scrapper.py
+++ scrapper.py
@@ -10,8 +10,7 @@ CURRENCIES = {
     "GBP": "Libra Esterlina",
     "JYP": "Yuan Japonés",
     "MXN": "Pesos Mexicanos",
-    "USD": "Dólar Americano",
-    "MLC": "MLC :("
+    "USD": "Dólar Americano"
 }
 
 
@@ -58,7 +57,16 @@ def process_html(page_content):
         if not widget:
             print_error_message()
         else:
-            print(f"{'Moneda':>20}", f"{'Signo':>6}", f"{'Compra':>11}", f"{'Venta':>11}", f"{'~MLC':>9}")
+            headers = [
+                f"{'Moneda':>20}",
+                f"{'Signo':>6}",
+                f"{'Compra':>11}",
+                f"{'Venta':>11}",
+                f"{'~MLC':>9}",
+                f"{'~100 MLC':>16}",
+                f"{'Total CUP':>12}"
+            ]
+            print(" ".join(headers))
             mlc_rate = get_mlc_rate()
             for currency_sign, currency_name in CURRENCIES.items():
                 tr = widget.find_next("tr", class_=currency_sign)
@@ -68,7 +76,19 @@ def process_html(page_content):
                         buy_price = tds[2].text.strip()
                         sale_price = tds[3].text.strip()
                         mlc = float(buy_price)/mlc_rate
-                        print(f"{currency_name:>20}", f"{currency_sign:>6}", f"{buy_price:>11}", f"{sale_price:>11}", f"{mlc:>9.5f}")
+                        currency_amount = (100/mlc)
+                        total_cup = currency_amount * float(sale_price)
+                        row_items = [
+                            f"{currency_name:>20}",                        # currency name
+                            f"{currency_sign:>6}",                         # currency sigil
+                            f"{buy_price:>11}",                            # buy price
+                            f"{sale_price:>11}",                           # sale price
+                            f"{mlc:>9.5f}",                                # currency equivalent to MLC
+                            f"{currency_amount:>12.5f} {currency_sign}",   # total currency needed for 100 MLC
+                            f"{total_cup:>12.5f}",                         # total CUP needed for 100 MLC
+
+                        ]
+                        print(" ".join(row_items))
 
 
 ##################
@@ -78,14 +98,14 @@ def process_html(page_content):
 # First load environment settings
 load_env_selectors()
 
-print("{:#^61}".format(" Start parsing wait a few seconds "))
+print("{:#^91}".format(" Estamos parseando la información, espere unos segundos "))
 
 URL = env_selectors.get("base_url")
 page = None
 try:
     page = requests.get(URL)
 except Exception as e:
-    print_error_message("Some problem has occur, you need to reload.")
+    print_error_message("Ha ocurrido un problema, necesitas refrescar.")
     print("------------------------")
     print(e.__str__())
 
